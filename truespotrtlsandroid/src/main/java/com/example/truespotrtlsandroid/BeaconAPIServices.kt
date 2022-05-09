955315c911cc4e1d8412c3a650912c26374553b1
maruthapandi <k.maruthapandi@rifluxyss.com>
2022-05-09 20:35:39 +0530

    issue fixed test committed

--- truespotrtlsandroid/src/main/java/com/example/truespotrtlsandroid/BeaconAPIServices.kt
+++ truespotrtlsandroid/src/main/java/com/example/truespotrtlsandroid/BeaconAPIServices.kt
@@ -13,10 +13,10 @@ import rx.Observable
 interface BeaconAPIServices {
 
     @POST("/${API.Endpoint.authorization}")
-    fun authenticate(@Query("tenantId") tenantId: String?): Observable<Response<Authorization?>?>
+    fun authenticate(@Query("tenantId") tenantId: String?): Observable<Authorization?>
 
     @GET("/${API.Endpoint.applications}/?self")
-    fun getAppinfo(): Observable<Response<TSApplication?>?>
+    fun getAppinfo(): Observable<TSApplication?>
 
     @GET("/${API.Endpoint.applications}/?self")
     fun getTrackingDevices(): Observable<Response<TSApplication?>?>
