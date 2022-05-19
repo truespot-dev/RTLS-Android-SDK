4ac413d26ed125162db30b196f638f7fa32ddf0f
maruthapandi <k.maruthapandi@rifluxyss.com>
2022-05-19 10:22:31 +0530

    Location Manager Implementation(18-05-2022)

---
+++ truespotrtlsandroid/src/main/java/com/example/truespotrtlsandroid/Constant.java
@@ -0,0 +1,10 @@
+package com.example.truespotrtlsandroid;
+
+public class Constant {
+
+    public static class Location {
+        public static final int GPS_REQUEST = 5467;
+        public static final long LOCATION_REQUEST_INTERVAL_MILLIS = 5 * 1000;
+        public static final long LOCATION_UPDATE_PAUSE_TIMEOUT_MILLIS = 2000;
+    }
+}
--- truespotrtlsandroid/src/main/java/com/example/truespotrtlsandroid/TSLocationManager.kt
+++ truespotrtlsandroid/src/main/java/com/example/truespotrtlsandroid/TSLocationManager.kt
@@ -4,48 +4,56 @@ import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.os.Build
+import android.os.Handler
+import android.provider.Settings
+import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationServices
+import timber.log.Timber
 
-class TSLocationManager(context : Context,activity: Activity) {
-    val shared = TSLocationManager(context,activity)
+
+class TSLocationManager(context : Context, activity: Activity) {
+    //val shared = TSLocationManager(context,activity)
     val mContext = context
     val mActivity = activity
+    private var cLocationManager: CLLocationManager? = null
+    private var locationManager : LocationManager? = null
+    var currentLocation: Location? = null
+    private val handlerLocation: Handler? = null
+    protected var googleApiClient: GoogleApiClient? = null
+
 
     init {
+
         requestLocationPermission()
         Toast.makeText(mContext,"Location-->init", Toast.LENGTH_LONG).show()
 
     }
 
+
     fun requestLocationPermission()
     {
-        if(ContextCompat.checkSelfPermission(mContext,
-                Manifest.permission.ACCESS_FINE_LOCATION) !==
-            PackageManager.PERMISSION_GRANTED)
+        if(isLocationServiceEnabled(mContext))
         {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(mActivity,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-                ActivityCompat.requestPermissions(mActivity,
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
-            } else {
-                ActivityCompat.requestPermissions(mActivity,
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
-            }
+            cLocationManager = CLLocationManager(mContext,mActivity)
+            cLocationManager!!.startLocationUpdates()
         }
-    }
 
-    fun checkRequiredPermission(permission : String) : Boolean
-    {
-        return ContextCompat.checkSelfPermission(mContext, permission) == PackageManager.PERMISSION_GRANTED
     }
-
 
     fun startScanning()
     {
+        updateLocation(true)
 
-        if(checkRequiredPermission(Manifest.permission.ACCESS_FINE_LOCATION))
+        if(isLocationServiceEnabled(mContext))
         {
             Toast.makeText(mContext,"Location-->True", Toast.LENGTH_LONG).show()
         }
@@ -57,6 +65,47 @@ class TSLocationManager(context : Context,activity: Activity) {
 
     fun  stopScanning()
     {
+        updateLocation(false)
 
     }
+
+
+
+    private fun updateLocation(start: Boolean)
+    {
+
+        if(cLocationManager == null)
+        {
+            Timber.i("=====Location Manager NIL======")
+            return
+        }
+        if(start){
+            Timber.i("=====Location Manager Start Updating======")
+            cLocationManager!!.startLocationUpdates()
+
+        }
+        else
+        {
+            Timber.i("=====Location Manager Stop Updating======")
+            cLocationManager!!.stopLocationUpdates()
+        }
+
+    }
+
+    private fun isLocationServiceEnabled(context: Context): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            // This is a new method provided in API 28
+            val lm = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            lm.isLocationEnabled
+        } else {
+            // This was deprecated in API 28
+            val mode: Int = Settings.Secure.getInt(
+                context.contentResolver, Settings.Secure.LOCATION_MODE,
+                Settings.Secure.LOCATION_MODE_OFF
+            )
+            mode != Settings.Secure.LOCATION_MODE_OFF
+        }
+    }
+
+
 }
