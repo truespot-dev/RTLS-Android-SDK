e9391fddf50ee00dd9af4ba7961dabe3325fc556
maruthapandi <k.maruthapandi@rifluxyss.com>
2022-06-23 16:50:09 +0530

    Remove the demo Changes

--- app/src/main/java/com/example/truespotrtlsandroidsdk/BaseViewHolder.kt
+++
@@ -1,9 +0,0 @@
-package com.example.truespotrtlsandroidsdk
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-
-abstract class BaseViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-    abstract fun bind(item: Any,position:Int)
-}
