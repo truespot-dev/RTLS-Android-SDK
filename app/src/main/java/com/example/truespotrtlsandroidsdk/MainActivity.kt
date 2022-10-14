5bbade3f06ba17aa105b2e59b0cf93086ec17ef4
maruthapandi <k.maruthapandi@rifluxyss.com>
2022-10-14 15:17:30 +0530

    Demo (Functionality implemented)

--- app/src/main/java/com/example/truespotrtlsandroidsdk/MainActivity.kt
+++ app/src/main/java/com/example/truespotrtlsandroidsdk/MainActivity.kt
@@ -16,7 +16,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.truespotrtlsandroid.*
 import com.example.truespotrtlsandroid.CompletionCallBack
 import com.example.truespotrtlsandroid.beacon.TSBeaconSighting
+import com.example.truespotrtlsandroid.models.TSDevice
 import com.google.gson.Gson
+import java.lang.Exception
 import kotlin.Unit.toString
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +26,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        try {
+/*
+            TrueSpot.configure(this,"600efa6da13b840ab04ad9c2","1234567890",true){
+                if (it == null) {
+                    Log.i("Configure","Success")
+                    TrueSpot.startScanning(this)
+                    TrueSpot.launchTruedarMode(this@MainActivity.supportFragmentManager,"0000-11H1W")
+                } else {
+                    Log.i("Configure","Error:${it}")
+                    TrueSpot.startScanning(this)
+                    TrueSpot.launchTruedarMode(this@MainActivity.supportFragmentManager,"0000-11H1W")
+                }
+            }*/
+
+             TrueSpot.launchTruedarMode(this.supportFragmentManager,"0000-11H1W",this)
+
+        }catch (ex : Exception){
+            Log.i("Exception---->",ex.message.toString())
+        }
+
+
 
     }
 
